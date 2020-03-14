c0af3dd8cddc74c7dc3ac0bba2d1d6441470c581
Fang Ran <fangran@Fangs-MacBook-Pro.local>
2020-03-14 22:54:51 +0800

    new js files

---
+++ js/example1.js
@@ -0,0 +1,10 @@
+// Load 'opencv.js' assigning the value to the global variable 'cv'
+cv = require('./opencv.js')
+
+// Define a global variable 'Module' with a method 'onRuntimeInitialized':
+Module = {
+  onRuntimeInitialized() {
+    // this is our application:
+    console.log(cv.getBuildInformation())
+  }
+}
